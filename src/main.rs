c4fd3c7a88b93bf651cdb86912d7830f951a2539
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-04-05 19:57:13 -0400

    moar ideas

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,47 @@
 
 #![allow(
     unused_mut, dead_code, non_upper_case_globals, unused_variables,
-    unreachable_code,
+    unreachable_code, unused_assignments,
 )]
 
 mod data_mgmr;
+mod structs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let verbose_str = std::env::var("VERBOSE").unwrap_or("".into());
+    let is_verbose = verbose_str.len() > 0;
+
     let args: Vec<String> = std::env::args().collect();
     // Grab data
-    let data: serde_pickle::Value;
-    if let Some(pickle_file) = args.get(1) {
+    let data: serde_pickle::Value = if let Some(pickle_file) = args.get(1) {
+        println!("Reading sea of raw data from {}", &pickle_file);
         let pickle_bytes: Vec<u8> = std::fs::read(pickle_file)?;
         let de_options = serde_pickle::DeOptions::new()
                             .decode_strings()
                             .replace_recursive_structures()
                             .replace_unresolved_globals();
-        data = serde_pickle::value_from_reader(pickle_bytes.as_slice(), de_options)?;
+        serde_pickle::value_from_reader(pickle_bytes.as_slice(), de_options)?
     }
     else {
-        println!("Warning: Generating RANDOM gis data. Pass a path to pickled data to use real GIS data (eg \"data/raw-layer-data.pickle\" as arg1)");
-        println!("         Pass SEED_NONCE to set repeadable random data.");
-        data = data_mgmr::gen_rand_raw_layer_data();
+        return Err("Error: pass path to a .pickle file containing a dictionary of layers + GIS data within".into());
     };
 
-    println!("data = {:#?}", data);
+    if verbose_str.contains("data") {
+        println!("data = {:?}", data);
+    }
 
-    // We record some "entire system" assumptions here.
-    // These particular numbers came from:
-    //  - https://www.eia.gov/international/data/world/petroleum-and-other-liquids/annual-petroleum-and-other-liquids-production?pd=5&p=0000001&u=0&f=A&v=heatmap&a=-&i=none&vo=value&vb=34&t=C&g=00000000000000000000000000000000000000000000000001&l=249-ruvvvvvfvtvnvv1vrvvvvfvvvvvvfvvvou20evvvvvvvvvvnvvvs0008&s=94694400000&e=1609459200000&ev=true
-    //  - https://www.eia.gov/international/data/world/petroleum-and-other-liquids/annual-petroleum-and-other-liquids-production?pd=5&p=0000000000000000000000000000000000g&u=0&f=A&v=heatmap&a=-&i=none&vo=value&vb=170&t=C&g=00000000000000000000000000000000000000000000000001&l=249-ruvvvvvfvtvnvv1vrvvvvfvvvvvvfvvvou20evvvvvvvvvvnvvvs0008&s=94694400000&e=1609459200000&ev=true
-    const aoi_petroleum_annual_production_thousand_barrels_per_day: f64 = 19_036.0;
-    const aoi_petroleum_annual_consumption_thousand_barrels_per_day: f64 = 19_890.0;
-
-    // If the import is negative, AoI is net exporter. Math does not change.
-    let aoi_imported_petroleum_thousand_barrels_per_day = aoi_petroleum_annual_consumption_thousand_barrels_per_day - aoi_petroleum_annual_production_thousand_barrels_per_day;
-    let aoi_imported_petroleum_percent_of_cosumption = 100.0 * (aoi_imported_petroleum_thousand_barrels_per_day / aoi_petroleum_annual_consumption_thousand_barrels_per_day).abs();
-    if aoi_imported_petroleum_thousand_barrels_per_day > 0.0 {
-        println!("The AoI is a net Importer of petroleum; {} thousand barrels/day are imported. ({:.2}% of total consumption)",
-            aoi_imported_petroleum_thousand_barrels_per_day.abs(),
-            aoi_imported_petroleum_percent_of_cosumption
-        );
+    let assumptions: structs::AssumptionsToml = if let Some(assumptions_toml) = args.get(2) {
+        println!("Reading assumptions from {}", &assumptions_toml);
+        let assumptions_bytes: Vec<u8> = std::fs::read(assumptions_toml)?;
+        let assumptions_string = String::from_utf8_lossy(&assumptions_bytes);
+        toml::from_str(&assumptions_string)?
     }
     else {
-        println!("The AoI is a net Exporter of petroleum; {} thousand barrels/day are exported. ({:.2}% of total consumption)",
-            aoi_imported_petroleum_thousand_barrels_per_day.abs(),
-            aoi_imported_petroleum_percent_of_cosumption
-        );
+        return Err("Error: pass path to an assumptions.toml file containing AoI assumptions and meta-data".into());
+    };
+
+    if is_verbose {
+        println!("assumptions = {:#?}", assumptions);
     }
 
     // Turn the data into modelable items; we construct
@@ -65,3 +60,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/structs.rs
@@ -0,0 +1,75 @@
+
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct AssumptionsToml {
+  pub aoi_name: String,
+
+  pub aoi_min_map_pt: (f64, f64), // MinX, MinY
+  pub aoi_max_map_pt: (f64, f64), // MaxX, MaxY
+
+  pub aoi_annual_production: Vec<MeasuredProductAmount>,
+  pub aoi_annual_consumption: Vec<MeasuredProductAmount>,
+}
+
+pub type ProductID = u32;
+
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct Product {
+  pub id: ProductID,
+  pub name: String,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct MeasuredProductAmount {
+  pub product: ProductID,
+  pub thousand_barrels_per_day: f64,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct ConvertedProductCapability {
+  pub input_product: ProductID,
+  pub input_thousand_barrels_per_day: f64,
+  pub output_multiplier: f64, // if 1.0, output is perfect. If 0.5, for every 1000 barrels in 500 come out.
+  pub output_product: ProductID,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct ProductProducer {
+  pub name: String,
+  pub map_position: (f64, f64),
+  pub products: Vec<MeasuredProductAmount>, // How much is produced/day of operation?
+}
+
+
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct ProductConverter {
+  pub name: String,
+  pub map_position: (f64, f64),
+  pub products: Vec<ConvertedProductCapability>, // How much is converted/day of operation?
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct ProductConsumer {
+  pub name: String,
+  pub map_position: (f64, f64),
+  pub products: Vec<MeasuredProductAmount>, // How much is consumed/day of operation?
+}
+
+// This models eg an oil pipeline and makes endpoints + types easy to query
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct ProductFlow {
+  pub product: ProductID,
+  pub max_capacity_thousand_barrels_per_day: f64,
+  pub input_map_position: (f64, f64),
+  pub output_map_position: (f64, f64),
+  pub bidirectional: bool,
+  pub map_path: Vec<(f64, f64)>,
+}
+
+
+
+
+
+
+
+
+
