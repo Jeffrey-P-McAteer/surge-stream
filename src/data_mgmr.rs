262dbaf61ebb4f1fd7be3dc5f5358f56d6b64f3e
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-04-06 08:34:22 -0400

    nicer labels

--- src/data_mgmr.rs
+++ src/data_mgmr.rs
@@ -34,7 +34,7 @@ pub fn get_all_points(data_sea: &serde_pickle::Value) -> Vec<(f64, f64, String)>
                   else {
                     format!("{}", k)
                   };
-                  let v_string = format!("{:#?}", v);
+                  let v_string = format!("{:?}", v);
                   debug_s += &format!("{k_string}={v_string},");
                 }
               }
